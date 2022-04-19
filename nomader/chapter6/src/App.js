1fc61e7035d36ed5b9a034eace67acde4f18cd82
mihyun <mihy621@gmail.com>
2022-04-19 16:50:13 +0900

    06-01. feat: Effect Hook 사용하기

--- nomader/chapter6/src/App.js
+++ nomader/chapter6/src/App.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+const SearchBar = () => {
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("created😁");
+    return () => console.log("destroyed🥲"); // clean-up, component가 사라질 때 실행된다.
+  });
+  return (
+    <input
+      value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholder="Search here..."
+    ></input>
+  );
+};
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const showButton = () => setShowing((prev) => !prev);
+
+  console.log("i run all the time");
+  // 컴포넌트가 최초 렌더링 될 때 한 번만 실행
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+  // dependency로 설정한 counter의 상태가 변화될 때마다 effect를 실행
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+
   return (
     <div>
-      <h1>Hi, there!</h1>
+      {showing ? <SearchBar /> : null}
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
+      <button onClick={showButton}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
