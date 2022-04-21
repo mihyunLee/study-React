6e61f5d8748347e89707d8b4b4ab68c76fc4fcd6
mihyun <mihy621@gmail.com>
2022-04-21 22:08:00 +0900

    07-03. feat: 내가 가진 금액으로 선택한 코인에 따라 살 수 있는  개수를 보여주는 기능 추가

--- nomader/chapter7/src/CoinTracker.js
+++ nomader/chapter7/src/CoinTracker.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 const CoinTracker = () => {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState("");
+
   // 최초 1회만 실행
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=500")
@@ -12,17 +15,35 @@ const CoinTracker = () => {
         setLoading(false);
       });
   }, []);
+
+  const onChangeMoney = (event) => {
+    setMoney(event.target.value);
+  };
+  const onChangeSelect = (event) => {
+    setSelectedCoin(event.target[event.target.selectedIndex]);
+  };
+
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
+      <h1>The Coins! ({coins.length} coins)</h1>
+      <h2>Please enter the amount of money you have</h2>
+      <input value={money} onChange={onChangeMoney} type="number" />$
+      <p>{loading ? <strong>Loading...</strong> : null}</p>
+      <select onChange={onChangeSelect}>
+        <option>Select Coin</option>
         {coins.map((coin) => (
-          <li key={coin.id}>
+          <option key={coin.id} value={`${coin.name} (${coin.symbol})`}>
             {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </li>
+          </option>
         ))}
-      </ul>
+      </select>
+      <h3>
+        You can get{" "}
+        <span style={{ color: "tomato" }}>
+          {money === 0 || money / selectedCoin.text.split(" ").slice(-2)[0]}
+          {selectedCoin.value === "Select Coin" || selectedCoin.value}
+        </span>
+      </h3>
     </div>
   );
 };
