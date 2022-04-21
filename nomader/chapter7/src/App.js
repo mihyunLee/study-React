c36755d87fdf77ccdcd8074116c7ebe53ebf230e
mihyun <mihy621@gmail.com>
2022-04-21 16:04:24 +0900

    07-01. docs: ToDoList Component 분리

--- nomader/chapter7/src/App.js
+++ nomader/chapter7/src/App.js
@@ -1,40 +1,9 @@
-import { useState } from "react";
+import ToDoList from "./ToDoList";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-
-  console.log(toDos);
-
   return (
     <div>
-      <h1>My To Do List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          palceholder="Write your to do..."
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((toDo, index) => (
-          <li key={index}>{toDo}</li>
-        ))}
-      </ul>
+      <ToDoList />
     </div>
   );
 }
