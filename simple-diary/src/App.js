323c1848275e2330ee8fe4f590067ddedcd59338
mihyun <mihy621@gmail.com>
2022-05-12 16:53:46 +0900

    feat: 다이어리 리스트를 렌더링하는  컴포넌트 생성

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "myoni",
+    content: "hihihi~",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "kim",
+    content: "i'm kim!",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "lee",
+    content: "i'm lee!",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
