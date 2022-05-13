e88a9e3e0abcf20733ae7b50db9d458568465c2e
mihyunLee <mihy621@gmail.com>
2022-05-14 00:33:44 +0900

    modify: onDelete() -> onRemove()로 변경

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -22,7 +22,7 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}번째 일기가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} diaryList={data} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
