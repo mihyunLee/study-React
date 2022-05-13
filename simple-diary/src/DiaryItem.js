908e4240a4a6ffb752c618b32403a010a6f9852f
mihyunLee <mihy621@gmail.com>
2022-05-14 01:06:20 +0900

    feat: 다이어리 데이터 수정 구현

--- simple-diary/src/DiaryItem.js
+++ simple-diary/src/DiaryItem.js
@@ -1,4 +1,7 @@
+import { useRef, useState } from "react";
+
 const DiaryItem = ({
+  onEdit,
   onRemove,
   id,
   author,
@@ -6,6 +9,36 @@ const DiaryItem = ({
   created_date,
   emotion,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [localCotent, setLocalContent] = useState(content);
+
+  const localContentInput = useRef();
+
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleEdit = () => {
+    if (localCotent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localCotent);
+      toggleIsEdit();
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -15,16 +48,30 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onRemove(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localCotent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleEdit}>수정 완료</button>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+        </>
+      ) : (
+        <>
+          <button onClick={toggleIsEdit}>수정하기</button>
+          <button onClick={handleRemove}>삭제하기</button>
+        </>
+      )}
     </div>
   );
 };
--- simple-diary/src/DiaryList.js
+++ simple-diary/src/DiaryList.js
@@ -1,13 +1,13 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onRemove, diaryList }) => {
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h1>DiaryList</h1>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} onRemove={onRemove} />
+          <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
         ))}
       </div>
     </div>
