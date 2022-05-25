9c3e387b35b7cb4ecadd5d2dfb07664de8602211
mihyunLee <mihy621@gmail.com>
2022-05-25 22:49:49 +0900

    feat: React Router 연습

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import RouteTest from "./components/RouteTest";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Hello, React!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+        <RouteTest></RouteTest>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ emotion-diary/src/pages/Edit.js
@@ -0,0 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
+  console.log(`id: ${id} mode: ${mode}`);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "mihyun" })}>바꾸기</button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        home으로 이동하기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
