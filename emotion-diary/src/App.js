a48d50e259b81bc1032c7b7a199cc13d3782bce7
mihyunLee <mihy621@gmail.com>
2022-05-26 14:55:54 +0900

    feat: 기본 레이아웃, 공통 컴포넌트 추가

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -6,20 +6,44 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header
+          headText={"App"}
+          leftChild={
+            <Button text="왼쪽 버튼" onClick={() => alert("왼쪽 버튼")} />
+          }
+          rightChild={
+            <Button text="오른쪽 버튼" onClick={() => alert("오른쪽 버튼")} />
+          }
+        />
         <h1>Hello, React!</h1>
+        <Button
+          text={"버튼"}
+          type={"default"}
+          onClick={() => alert("버튼 클릭!")}
+        />
+        <Button
+          text={"버튼"}
+          type={"negative"}
+          onClick={() => alert("버튼 클릭!")}
+        />
+        <Button
+          text={"버튼"}
+          type={"positive"}
+          onClick={() => alert("버튼 클릭!")}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest></RouteTest>
       </div>
     </BrowserRouter>
   );
