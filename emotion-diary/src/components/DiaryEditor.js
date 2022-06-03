99de7a35023a8015e8abadb758386cc8266dbc54
mihyunLee <mihy621@gmail.com>
2022-06-04 01:27:32 +0900

    feat: 다이어리 삭제 버튼 추가

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -16,7 +16,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
     getStringDate(new Date(+new Date() + 3240 * 10000))
   );
 
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
 
   const contentRef = useRef();
 
@@ -47,6 +47,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate("/", { replace: true });
   };
 
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       setDate(getStringDate(new Date(originData.date)));
@@ -60,6 +67,15 @@ const DiaryEditor = ({ isEdit, originData }) => {
       <Header
         headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={<Button text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
+        rightChild={
+          isEdit && (
+            <Button
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
+        }
       />
       <div>
         <section>
