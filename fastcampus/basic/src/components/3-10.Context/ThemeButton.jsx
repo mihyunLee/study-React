adabd11c8a03e69fcf2eac2836f8b1ab1f65d555
mihyunLee <mihy621@gmail.com>
2022-10-26 19:46:11 +0900

    3-10. Context (Functional Component)

--- fastcampus/basic/src/components/3-10.Context/ThemeButton.jsx
+++ fastcampus/basic/src/components/3-10.Context/ThemeButton.jsx
@@ -1,22 +1,15 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 
-class ThemeButton extends Component {
-  render() {
-    let props = this.props;
-    let theme = this.context;
-    return (
-      <button
-        {...props}
-        onClick={props.changeTheme}
-        style={{ backgroundColor: theme.background, color: theme.foreground }}
-      >
-        Button
-      </button>
-    );
-  }
+export default function ThemeButton(props) {
+  const theme = useContext(ThemeContext);
+  return (
+    <button
+      {...props}
+      onClick={props.changeTheme}
+      style={{ backgroundColor: theme.background, color: theme.foreground }}
+    >
+      Button
+    </button>
+  );
 }
-
-ThemeButton.contextType = ThemeContext;
-
-export default ThemeButton;
