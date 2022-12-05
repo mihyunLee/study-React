b42a3f5d160b769c54138658d4a5d1704f1452bc
mihyunLee <mihy621@gmail.com>
2022-12-05 16:57:29 +0900

    Data Fetching Library. SWR

---
+++ fastcampus/data-fetching/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Profile from "./components/SWRExample/Profile";
+import Cache from "./components/SWRExample/Cache";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Cache />
+    </div>
+  );
+}
+
+export default App;
---
+++ fastcampus/data-fetching/src/components/SWRExample/Profile.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = (...args) => axios.get(...args).then((res) => res.data);
+
+function useUser(id) {
+  const { data, error } = useSWR(`/api/user/${id}`, fetcher);
+
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
+
+export default function Page() {
+  // 데이터 렌더링
+  return (
+    <>
+      <Profile id={124} />
+      <Avatar id={124} />
+    </>
+  );
+}
+
+function Profile({ id }) {
+  const { user, isLoading, isError } = useUser(id);
+
+  if (isError) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  // 데이터 렌더링
+  return (
+    <>
+      <div>hello {user.name}!</div>
+    </>
+  );
+}
+
+export function Avatar({ id }) {
+  const { user, isLoading, isError } = useUser(id);
+
+  if (isError) return <div>failed to load (Avatar)</div>;
+  if (isLoading) return <div>loading... (Avatar)</div>;
+
+  // 데이터 렌더링
+  return <div>hello {user.name}! (Avatar) </div>;
+}
