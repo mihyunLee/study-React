092ae375a6db3dfe883b44d8db7a390c9d8e2210
mihyunLee <mihy621@gmail.com>
2022-12-16 15:58:14 +0900

    GraphQL(2) - GraphQL Server: 뮤테이션 리졸버 설정

--- fastcampus/GraphQL/apollo-example/fullstack-tutorial/start/server/src/resolvers.js
+++ fastcampus/GraphQL/apollo-example/fullstack-tutorial/start/server/src/resolvers.js
@@ -1,6 +1,7 @@
 const { paginateResults } = require("./utils");
 
 module.exports = {
+  // 쿼리 리졸버
   Query: {
     launches: async (_, { pageSize = 20, after }, { dataSources }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
@@ -56,6 +57,50 @@ module.exports = {
           launchIds,
         }) || []
       );
+    },
+  },
+
+  // 뮤테이션 리졸버
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        user.token = Buffer.from(email).toString("base64");
+        return user;
+      }
+    },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? "trips booked successfully"
+            : `the following launches couldn't be booked: ${launchIds.filter(
+                (id) => !results.includes(id)
+              )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result)
+        return {
+          success: false,
+          message: "failed to cancel trip",
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: "trip cancelled",
+        launches: [launch],
+      };
     },
   },
 };
